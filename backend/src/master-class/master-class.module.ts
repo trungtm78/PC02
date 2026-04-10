8962bba2d7daea8aeaaa3904389412ad5dee2dc6
TRUNG-PC\Than Minh Trung <trungtm@aipower.vn>
2026-04-11 06:17:09 +0700

    fix: import AuditModule trong MasterClassModule

--- backend/src/master-class/master-class.module.ts
+++ backend/src/master-class/master-class.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuditModule } from '../audit/audit.module';
 import { MasterClassController } from './master-class.controller';
 import { MasterClassService } from './master-class.service';
 
 @Module({
+  imports: [AuditModule],
   controllers: [MasterClassController],
   providers: [MasterClassService],
   exports: [MasterClassService],
